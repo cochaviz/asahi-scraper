971dc3003727c855a01f10461ca1c5dc93d3e3e2
zoharcochavi <zohar.cochavi@gmail.com>
2021-04-20 13:03:46 +0200

    Headline-only search

---
+++ yomiuri-scraper.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+
+import re
+import sys
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.common.exceptions import NoSuchElementException
+
+
+def parse_dates(filename, username, passwd, search_query):
+    url = 'https://login.ezproxy.leidenuniv.nl/login?qurl=https://database.yomiuri.co.jp%2frekishikan%2f'
+
+    # Start
+    driver = webdriver.Chrome("chromedriver")
+    driver.get(url)
+
+    print("Succesfully found and started the chromedriver")
+    print("User query: " + search_query)
+
+    # -- LEIDEN UNIV ---
+    print("Loggin in to leidenuniv...")
+    
+    # SSO
+    login_button = driver.find_element_by_id('sso')
+    login_button.click()
+
+    # Enter creds
+    username_field = driver.find_element_by_name('Ecom_User_ID')
+    passwd_field = driver.find_element_by_name('Ecom_Password')
+
+    username_field.send_keys(username)
+    passwd_field.send_keys(passwd)
+
+    # Wait for second login button
+    driver.implicitly_wait(5)
+    login_button = driver.find_element_by_id('loginbtn')
+    login_button.click()
+
+    print("Successful!")
+
+    # --- yomiuri ---
+    print("Entered yomiuri!")
+
+    # Yomiuri uses iframes for each of the web elements, bruh
+    search_iframe_name = 'frame_bun'
+    search_iframe = driver.find_element_by_name(search_iframe_name)
+    driver.switch_to.frame(search_iframe)
+
+    # English
+    login_xpath = '//*[@id="btnEnglish"]/a'
+    login_button = driver.find_element_by_xpath(login_xpath)
+    login_button.click()
+  
+    # 1986 onwards
+    login_xpath = '/html/body/div[2]/div[4]/div/ul/li[4]/a'
+    login_button = driver.find_element_by_xpath(login_xpath)
+    login_button.click()
+
+    print("Entering query...")
+
+    # Enter query
+    query_field= driver.find_element_by_id('txtWord_ID')
+    query_field.send_keys(search_query)
+
+    # Set number of search results
+    action = ActionChains(driver);
+    n_results = driver.find_element_by_name('sortPageDto.offset')
+
+    print("Successful!")
+
+    # Search
+    driver.implicitly_wait(5)
+    search_button = driver.find_element_by_id('rdoSrchMode2') 
+
+    # Perform!
+    action.move_to_element(n_results).click()\
+          .pause(1)\
+          .move_to_element(search_button).click().perform()
+
+    print("Searching and parsing...\n")
+
+    # Regex expression for extracting dates
+    page = 1
+    date_expression = '([0-9]{4})\.([0-9]{2})\.([0-9]{2})\.'
+
+    # Open output file
+    myfile = open(filename + ".csv", 'a', newline='')
+    print("Opened file: " + filename + ".csv")
+    file_writer = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+
+    # Now cycle through the pages and extract the dates
+    while True:
+        # User feedback
+        print("Reading page " + str(page) + "...")
+        
+
+        # Get elements
+        matched_element = driver.find_element_by_class("marker_snippet1")
+        
+        # Collapse the list
+        title_list = map(lambda e : e.find_element_by_xpath("..").text, matched_elements)
+        raw_date_list = map(lambda e : e.find_element_by_xpath("..").find_element_by_xpath("..").text, matched_elements)
+
+        # Filter date from full element
+        dates = []
+
+        for text in raw_date_list:
+            match = re.match(date_expression, text)
+            # If it matches, format and append
+            if match:
+                dates.append(match.group(3) + "-" + match.group(2) + "-" + match.group(1))
+
+        zip(title_list, dates)
+
+        # Write matches to file
+        print("Appending to file...")
+        print(dates, "\n")
+        file_writer.writerows(dates)
+       
+        # Try to get next page
+        next = getNext(driver)
+        
+        # Return if we don't have anymore
+        if not next:
+            break;
+
+        # Continue to next page
+        page += 1
+        next.click()
+
+
+def getNext(driver):
+    next = None
+
+    try:
+        next = driver.find_element_by_class('nextPage') 
+    except NoSuchElementException:
+        pass
+    finally:
+        return next
+
+
+def create_file(filename="out"):
+    with open(filename + ".csv", 'w', newline='') as myfile:
+     print("Created " + filename + ".csv")
+
+    return filename
+
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print("usage: asahi-scraper <username> <passwd> optional:<file_name>")
+        exit(1)
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    
+    search_query = "国産"
+    
+    file = None
+    filename = None
+    
+    if len(sys.argv) > 3:
+        filename = create_file(filename=sys.argv[3])
+    else:
+        filename = create_file()
+
+    parse_dates(filename, username, passwd, search_query)
+    
+    exit(0)
+    
